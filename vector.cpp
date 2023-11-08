e471b96f6a4d9d3662ec0b3bdb36e9a65a7d934f
Azizul Islam <contact.azizulislam@gmail.com>
2023-11-08 09:26:25 +0600

    Vector

--- vector.cpp
+++ vector.cpp
@@ -4,24 +4,26 @@ using namespace std;
 
 int main()
 {
-    vector<int> v; // basic declaration syntax
-    vector<int> a(4); //vector declaration with fixed size
-    vector<int> x(4,2); //vector declaration as fixed size and initialize with 1 (like this: 2 2 2 2)
-    vector<int> last(x); //this will copy the elements of x
+    vector<int> v;       // basic declaration syntax
+    vector<int> a(4);    // vector declaration with fixed size
+    vector<int> x(4, 2); // vector declaration as fixed size and initialize with 1 (like this: 2 2 2 2)
+    vector<int> last(x); // this will copy the elements of x
 
     for (int i = 0; i < 5; i++) // taking input
     {
-        int n; cin >> n;
-        v.push_back(n); //insert data or elements at the end of the vector
+        int n;
+        cin >> n;
+        v.push_back(n); // insert data or elements at the end of the vector
     }
 
-    cout << "Capacity: " << v.capacity() << endl; //checking capacity of declared vector
-    v.pop_back(); //this will poped last element
+    cout << "Capacity: " << v.capacity() << endl; // checking capacity of declared vector
+    v.pop_back();                                 // this will poped last element
+    v.clear();                                    // this can completely empty the element of declared vector
 
     for (int i = 0; i < last.size(); i++) // printing output
     {
         cout << last[i] << " ";
     }
 
-    v.clear(); //this can completely empty the element of declared vector
+    return 0;
 }
