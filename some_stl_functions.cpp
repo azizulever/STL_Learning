9158c50fe1116f4b4f4b1cc73ff014daa2043d62
Azizul Islam <111518588+azizulever@users.noreply.github.com>
2023-11-20 11:36:43 +0600

    Some STL Functions

--- some_stl_functions.cpp
+++ some_stl_functions.cpp
@@ -1,6 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    
+int main()
+{
+    vector<int> v;
+    v.push_back(5);
+    v.push_back(9);
+    v.push_back(1);
+    v.push_back(7);
+
+    int sz = v.size();        // vector size
+    sort(v.begin(), v.end()); // sort, Time complexity-> O(NlogN)
+
+    cout << binary_search(v.begin(), v.end(), 3) << endl; // binary search stl, Time Complexity O(logN)
 }
